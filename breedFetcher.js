0b4fd1ebd733550eb4ba22dc98c8c05680940f24
Soliloquy <ibrahia@ucalgary.ca>
2021-06-03 21:03:15 +0000

    Remove else statements

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,12 +6,13 @@ const breed = argv[0];
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   //if search not found, length of body is equal to 2
   if (body.length === 2) {
-    console.log('Requested breed not found');
-  } else if (error) {
-    console.log('Error, Request Failed: ' + error);
-  } else {
-    //Convert string to object type
-    const data = JSON.parse(body);
-    console.log(data[0]['description']);
+    return console.log('Requested breed not found');
   }
+  if (error) {
+    return console.log('Error, Request Failed; ' + error);
+  }
+  //Convert string to object type
+  const data = JSON.parse(body);
+  console.log(data[0]['description']);
+
 });
