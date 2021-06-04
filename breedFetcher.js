eecb0550731c72acae5635dd2e8da3a21a28702b
Soliloquy <ibrahia@ucalgary.ca>
2021-06-04 00:24:28 +0000

    Utilizing asychronous callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const request = require('request');
 
-const argv = process.argv.slice(2);
-const breed = argv[0];
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  //if search not found, length of body is equal to 2
-  if (body.length === 2) {
-    return console.log('Requested breed not found');
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  //check for error first  
   if (error) {
-    return console.log('Error, Request Failed; ' + error);
-  }
-  //Convert string to object type
-  const data = JSON.parse(body);
-  console.log(data[0]['description']);
+      callback(error, null);
+      return;
+    }
+  
+  //if search not found, length of body is equal to 2
+    if (body.length === 2) {
+      callback(null, 'Invalid search, please enter a correct breed');
+      return;
+    }
 
-});
+    const data = JSON.parse(body)[0]['description'];
+    callback(null, data);
+  });
+};
+
+module.exports = { fetchBreedDescription };
